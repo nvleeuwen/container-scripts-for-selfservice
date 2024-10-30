13580db55f64a4a96816bde6497d763295ee2c13
Tobias Fenster <tfenster@4psbau.de>
2024-10-30 15:36:15 +0100

    re-enable

--- misc/AdditionalSetupPrerequisites.ps1
+++ misc/AdditionalSetupPrerequisites.ps1
@@ -1,7 +1,6 @@
 Import-Module (Join-Path $PSScriptRoot "helper\k8s-bc-helper.psd1")
 Install-OpenSSH
 if ($env:IsBuildContainer) {
-    # seems to kill output log handling in the pipeline
-    #Install-Chocolatey
-    #Install-Nodejs
+    Install-Chocolatey
+    Install-Nodejs
 }
